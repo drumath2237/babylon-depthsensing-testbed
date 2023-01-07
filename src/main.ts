3b252f9b31f15c42ac269d452fedebef880b1818
drumath2237 <2237k.drumath@gmail.com>
2023-01-08 01:27:28 +0900

    :sparkles: babylonjs coreのlinkと動作確認

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./style.scss";
 
+import { Engine, MeshBuilder, Scene } from "@babylonjs/core";
+
 const main = () => {
   const renderCanvas = document.getElementById(
     "renderCanvas"
@@ -11,7 +13,15 @@ const main = () => {
   renderCanvas.width = document.documentElement.clientWidth;
   renderCanvas.height = document.documentElement.clientHeight;
 
-  console.log("Hello");
+  const engine = new Engine(renderCanvas, true);
+  const scene = new Scene(engine);
+
+  scene.createDefaultCameraOrLight(true, true, true);
+  MeshBuilder.CreateBox("box", { size: 0.2 }, scene);
+
+  engine.runRenderLoop(() => {
+    scene.render();
+  });
 };
 
 main();
